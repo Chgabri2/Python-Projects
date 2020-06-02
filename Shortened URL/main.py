bea1bafa74f5d9243def7d1704d28d036040faa7
Chgabri2 <52176942+Chgabri2@users.noreply.github.com>
2020-06-02 18:01:56 +0300

    Update main.py
    הי! הוספתי את החלק שלי
    בנוסך שיניתי את השם של המיין שלך כדי לתת אופצית בחירה בהתחלה אם לקבל כתובת חדשה או להשתמש בכתובת קיימת

--- Shortened URL/main.py
+++ Shortened URL/main.py
@@ -47,16 +47,44 @@ def handle_user_request(user_url):
         add_new_url_to_dict(user_url)
         print('Your new shortened url: %s ' % (all_data_dict[user_url]))
 
-
-def main():
+# old main- I change it to add the option to go to the url
+def get_new_url():
     global all_data_dict
     all_data_dict = import_data_to_dict()  # now I have all the file data in a dict
     user_url = (input("Enter your url: ")).replace("'", "")
     # now I need to check if the url already exists in my dict
     handle_user_request(user_url)
     return_data_to_file(all_data_dict)
-
-
+    
+    
+#go to the shorten url    
+def go_to_url():
+    adress = input("enter the url:\n")
+    if not adress.startswith('"our_domain/'):
+       print("error! url not legal")
+    adress = adress[len("our_domain/ "):-1]
+    for key in all_data_dict.keys():
+        if adress == all_data_dict[key]:
+            my_url = key
+            webbrowser.open(my_url)
+            return
+    print("error! unknown adress")
+    
+#The user can choose if he want to get a new url or to use one
+def main() :
+    while 1:
+        choice=input("choose 1 get a new url \nchoose 2 to use your new url \nchoose 3 to exit\n")
+        if int(choice) == 1:
+            get_new_url()
+        elif int(choice) == 2:
+            go_to_url()
+            break
+        elif int(choice) == 0:
+            break
+        else:
+            print("error! please enter 1,2 or 3")
+            continue
+            
 if __name__ == '__main__':
     main()
 
