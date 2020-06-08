d77fc36be7ee8e765cace45223e18226507f96c2
Chgabri2 <52176942+Chgabri2@users.noreply.github.com>
2020-06-08 22:34:20 +0300

    Update main.py
    שיניתי את הסדר של המילון
    מחקתי את המיין ואת הפונקציה השניה

--- Shortened URL/main.py
+++ Shortened URL/main.py
@@ -19,14 +19,16 @@ def return_data_to_file(data_dict):
 
 
 def url_exist(user_url):
-    if user_url in all_data_dict.keys():
+    if user_url in all_data_dict.values():
         return True
     else:
         return False
 
 
 def get_shorten_url_from_dict(user_url):
-    return all_data_dict[user_url]
+    for key in all_data_dict.keys():
+        if all_data_dict[key] == user_url:
+            return key
 
 
 def shortened_url():
@@ -35,7 +37,7 @@ def shortened_url():
 
 
 def add_new_url_to_dict(user_url):
-    all_data_dict[user_url] = 'our_domain/'+shortened_url()
+    all_data_dict[shortened_url()] = user_url
 
 
 def handle_user_request(user_url):
@@ -50,36 +52,6 @@ def get_new_url():
     user_url = (input("Enter your url: ")).replace('"', '')  # supposed to remove the unnecessary quotes
     handle_user_request(user_url)
     return_data_to_file(all_data_dict)
-
- 
-def go_to_url():
-    address = (input("enter the url:  ")).replace('"', '')
-    if not address.startswith('our_domain/'):
-        print("error! url not legal")
-    else:
-        for key in all_data_dict.keys():
-            if address == all_data_dict[key]:
-                my_url = key
-                webbrowser.open(my_url)
-                return
-            print("error! unknown address")
-
-
-def main():
-    global all_data_dict
-    all_data_dict = import_data_to_dict()
-    while 1:
-        choice = int(input("choose 1 get a new url \nchoose 2 to use your new url \nchoose 3 to exit\n"))
-        if choice == 1:
-            get_new_url()
-        elif choice == 2:
-            go_to_url()
-            break
-        elif choice == 0:
-            break
-        else:
-            print("error! please enter 1,2 or 3")
-            continue
 
 
 if __name__ == '__main__':
